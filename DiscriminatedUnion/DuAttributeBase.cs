de7c40b250af05e7189bbc62ab8491d353b5dc02
Nick Strupat <nick.strupat@gmail.com>
2026-02-04 22:24:34 -0500

    don't need the base attribute type

--- DiscriminatedUnion/DuAttributeBase.cs
+++
@@ -1,9 +0,0 @@
-namespace NickStrupat;
-
-[AttributeUsage(AttributeTargets.Class)]
-public class DuAttributeBase : Attribute
-{
-	private protected DuAttributeBase(Type[] types) => this.types = types;
-	private readonly ReadOnlyMemory<Type> types;
-	internal ReadOnlySpan<Type> Types => types.Span;
-}
