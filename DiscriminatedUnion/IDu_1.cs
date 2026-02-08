86984dc1b58b57b72f4cdef869c669cf62b615be
Nick Strupat <nick.strupat@gmail.com>
2026-02-08 18:59:12 -0500

    fix TryPick

--- DiscriminatedUnion/IDu_1.cs
+++ DiscriminatedUnion/IDu_1.cs
@@ -1,10 +1,11 @@
+using System.Diagnostics.CodeAnalysis;
 using System.Text.Json;
 
 namespace NickStrupat;
 
 public interface IDu<TDu> : IDu where TDu : IDu<TDu>
 {
-	public Boolean TryPick<T>(out T matched) where T : notnull;
+	public Boolean TryPick<T>([NotNullWhen(true)] out T? matched) where T : notnull;
 	void Visit<T>(Action<T> action) where T : notnull;
 
 	static virtual TDu Deserialize(ref Utf8JsonReader reader, JsonSerializerOptions options) => throw new NotImplementedException();
