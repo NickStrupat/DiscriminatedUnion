5754bca5283c58db49e38a083ba02dee0861fc1f
Nick Strupat <nick.strupat@gmail.com>
2026-03-01 21:11:57 -0500

    move AcceptTypes method from IDu_1 to IDu

--- DiscriminatedUnion/IDu.cs
+++ DiscriminatedUnion/IDu.cs
@@ -6,5 +6,10 @@ public interface IDu
 {
 	TResult Accept<TVisitor, TResult>(TVisitor visitor) where TVisitor : IVisitor<TResult> => Accept<TVisitor, TResult>(ref visitor);
 	TResult Accept<TVisitor, TResult>(ref TVisitor visitor) where TVisitor : IVisitor<TResult>;
+
+	static virtual void AcceptTypes<TTypeVisitor, TRefParam>(ref TTypeVisitor visitor, ref TRefParam refParam)
+	where TTypeVisitor : ITypeVisitor<TRefParam>
+	where TRefParam : allows ref struct => throw new NotImplementedException();
+
 	static virtual ImmutableArray<Type> Types => throw new NotImplementedException();
 }
--- DiscriminatedUnion/IDu_1.cs
+++ DiscriminatedUnion/IDu_1.cs
@@ -4,10 +4,6 @@ namespace NickStrupat;
 
 public interface IDu<TDu> : IDu where TDu : IDu<TDu>
 {
-	static abstract void AcceptTypes<TTypeVisitor, TRefParam>(ref TTypeVisitor visitor, ref TRefParam refParam)
-		where TTypeVisitor : ITypeVisitor<TRefParam>
-		where TRefParam : allows ref struct;
-
 	static abstract Boolean TryCreate<T>(T value, [NotNullWhen(true)] out TDu? du);
 	static virtual TDu Create<T>(T value) => TDu.TryCreate(value, out var du) ? du : throw new InvalidOperationException($"The value of type {typeof(T).FullName} cannot be converted to the discriminated union.");
 }
