6906d81b5cf9393f4313af606dcf250132665177
Nick Strupat <nick.strupat@gmail.com>
2026-03-02 19:45:30 -0500

    make ITypeVisitor.Initialize more generic

--- DiscriminatedUnion/Visitors/ITypeVisitor.cs
+++ DiscriminatedUnion/Visitors/ITypeVisitor.cs
@@ -2,6 +2,6 @@ namespace NickStrupat;
 
 public interface ITypeVisitor<TRefParam> where TRefParam : allows ref struct
 {
-	void Initialize(Int32 typeCount) {}
+	void Initialize<TDu>() where TDu : IDu {}
 	Boolean VisitType<T>(ref TRefParam refParam) where T : notnull;
 }
